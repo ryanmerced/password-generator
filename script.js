18f6cea3733c6aece5831eeac390b9e11b11fcb4
ryanmerced <ryan_merced@hotmail.com>
2022-06-13 15:43:41 -0700

    adding start of codes

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = [" !#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("")];
+var lowercase = ["abcdefghijklmnopqrstuvwxyz".split("")];
+var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")];
+
+function generatePassword() {
+  //prompt the user for their password lenght and store into a variable
+  //validate the password length and make sure it is = to 8 and 128 (if statement)
+  //prompt the user if they want special characters and store into a variable
+
+};
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
